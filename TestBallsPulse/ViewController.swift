e4704dfeab01274bf01b402713ba7086e595c0f3
Luciano <passos.luciano@outlook.com>
2018-08-28 23:42:20 -0300

    Initial Commit

---
+++ TestBallsPulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestBallsPulse
+//
+//  Created by Luciano Almeida on 28/08/18.
+//  Copyright © 2018 Luciano Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
